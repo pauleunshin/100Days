8acff088f988c35b228410ee9fc9b6d3ff4a8fc8
Paul <paul.eun.shin@gmail.com>
2023-02-20 08:00:55 -0800

    oop espresso final

--- OOP Espresso/main.py
+++ OOP Espresso/main.py
@@ -1,1 +1,30 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+powered = True
+
+
+
+
+while powered:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}) \n")
+    if choice.lower() == "off":
+        powered = False
+    elif choice.lower() == "report":
+        money_machine.report()
+        coffee_maker.report()
+    else:
+        drink = menu.find_drink(choice.lower())
+        resource = coffee_maker.is_resource_sufficient(drink)
+        if resource:
+            payment = money_machine.make_payment(drink.cost)
+            if payment:
+                coffee_maker.make_coffee(drink)
+
+
 
