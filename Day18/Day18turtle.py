97f6f970cb4a04b4274926c4e20319c20cb88faf
Paul <paul.eun.shin@gmail.com>
2023-02-22 22:37:51 -0800

    Day 18 Turtle cont

--- Day18/Day18turtle.py
+++ Day18/Day18turtle.py
@@ -7,15 +7,47 @@
 import turtle as t
 import random
 tim=t.Turtle()
-colours = ["CornflowerBlue"]
+t.colormode(255)
 
-def draw_shape(num_sides):
-    angle = 360/num_sides
-    for _ in range(num_sides):
-        tim.forward(100)
-        tim.right(angle)
+##Square to Decagon
+# colours = ["CornflowerBlue"]
 
-for shape_side_n in range(3, 11):
-    tim.color(random.choice(colours))
-    draw_shape(shape_side_n)
+# def draw_shape(num_sides):
+#     angle = 360/num_sides
+#     for _ in range(num_sides):
+#         tim.forward(100)
+#         tim.right(angle)
+
+# for shape_side_n in range(3, 11):
+#     tim.color(random.choice(colours))
+#     draw_shape(shape_side_n)
+
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    rgb = (r, g, b)
+    return rgb
+
+##Random Directions
+# directions = [0, 90, 180, 270]
+# tim.pensize(15)
+# tim.speed("fastest")
+#
+# for _ in range(200):
+#     tim.color(random_color())
+#     tim.forward(30)
+#     tim.setheading(random.choice(directions))
+
+tim.speed("fastest")
+def draw_spirograph(gap_size):
+    for _ in range(int(360/gap_size)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + 10)
+
+
+draw_spirograph(10)
+screen = t.Screen()
+screen.exitonclick()
 
