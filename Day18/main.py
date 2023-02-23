4a518a4f9184aa9673fe597107e6054bb28b7e0a
Paul <paul.eun.shin@gmail.com>
2023-02-23 00:32:33 -0800

    Day 18 Turtle cont

--- Day18/main.py
+++ Day18/main.py
@@ -1,10 +1,43 @@
 ###This code will not work in repl.it as there is no access to the colorgram package here.###
 ##We talk about this in the video tutorials##
-import colorgram
+# import colorgram
+#
+# rgb_colors = []
+# colors = colorgram.extract('image.jpg', 30)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+#
+# print(rgb_colors)
 
-rgb_colors = []
-colors = colorgram.extract('image.jpg', 30)
-for color in colors:
-    rgb_colors.append(color.rgb)
+import turtle as t
+import random
+t.colormode(255)
 
-print(rgb_colors)
+
+pulled_colors = [(245, 243, 238), (246, 242, 244), (202, 164, 110), (240, 245, 241), (236, 239, 243), (149, 75, 50),
+                 (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73),
+                 (47, 121, 86), (73, 43, 35), (145, 178, 149), (14, 98, 70), (232, 176, 165), (160, 142, 158),
+                 (54, 45, 50), (101, 75, 77), (183, 205, 171), (36, 60, 74), (19, 86, 89), (82, 148, 129),
+                 (147, 17, 19), (27, 68, 102), (12, 70, 64), (107, 127, 153), (176, 192, 208), (168, 99, 102)]
+
+tim = t.Turtle()
+tim.home()
+homepoint = 0
+for _ in range (10)
+
+    for _ in range (10):
+        tim.dot(20,random.choice(pulled_colors))
+        tim.penup()
+        tim.forward(70)
+        tim.pendown()
+
+
+
+
+screen = t.Screen()
+screen.exitonclick()
+
