d277f4f0ade0abe7d45d6c561117767972391444
Paul <paul.eun.shin@gmail.com>
2023-03-07 01:27:06 -0800

    Day 19 Turtle Race start

---
+++ Day19/turtlerace.py
@@ -0,0 +1,27 @@
+from turtle import Turtle, Screen
+import random
+screen = Screen()
+timmy = Turtle()
+tommy = Turtle()
+jimmy = Turtle()
+tom = Turtle()
+tim = Turtle()
+
+timmy.color("red"), timmy.shape("turtle")
+tommy.color("blue"), tommy.shape("turtle")
+jimmy.color("green"), jimmy.shape("turtle")
+tom.color("yellow"), tom.shape("turtle")
+tim.color("purple"), tim.shape("turtle")
+
+
+def random_distance(turtle):
+    turtle.
+
+winner = ""
+while not winner:
+
+
+
+
+guess = screen.textinput("Guess", "Which color turtle do you think will win?")
+screen.exitonclick()
