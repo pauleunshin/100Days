5c5510ee0282eb0c21edd9835b4e352b6d42856d
Paul <paul.eun.shin@gmail.com>
2023-03-13 23:14:49 -0700

    Day 19 Turtle Race Finished

--- Day19/turtlerace.py
+++ Day19/turtlerace.py
@@ -7,21 +7,40 @@ jimmy = Turtle()
 tom = Turtle()
 tim = Turtle()
 
-timmy.color("red"), timmy.shape("turtle")
-tommy.color("blue"), tommy.shape("turtle")
-jimmy.color("green"), jimmy.shape("turtle")
-tom.color("yellow"), tom.shape("turtle")
-tim.color("purple"), tim.shape("turtle")
+timmy.color("red"), timmy.shape("turtle"), timmy.penup()
+tommy.color("blue"), tommy.shape("turtle"), tommy.penup()
+jimmy.color("green"), jimmy.shape("turtle"), jimmy.penup()
+tom.color("yellow"), tom.shape("turtle"), tom.penup()
+tim.color("purple"), tim.shape("turtle"), tim.penup()
+screen.setup(width=500, height=400)
+
 
 
 def random_distance(turtle):
-    turtle.
+    distance = random.randrange(45)
+    turtle.forward(distance)
 
+
+def starting_line(turtles):
+    height = -140
+    for turtle in turtles:
+        turtle.setpos(x=-250,y=height)
+        height += 70
+
+turtles = [timmy,tommy,jimmy,tom,tim]
+
+race_status = False
 winner = ""
-while not winner:
+starting_line(turtles)
+guess = screen.textinput(title="Guess", prompt="Which color turtle do you think will win?")
+while not race_status:
+    for turtle in turtles:
+        random_distance(turtle)
+        if turtle.xcor() > 240:
+            race_status = True
+            winner = turtle.pencolor()
 
-
-
-
-guess = screen.textinput("Guess", "Which color turtle do you think will win?")
-screen.exitonclick()
+if winner == guess:
+    print(f"You've won the bet! Congrats the {winner} turtle won the race!")
+else:
+    print(f"Too bad. The {winner} turtle won this time. Try again next time.")
