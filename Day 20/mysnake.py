56a1bdff148bcc4a88bc647224d3ca2cd4cd7969
Paul <paul.eun.shin@gmail.com>
2023-03-29 00:29:04 -0700

    Day 20 Snake cont current direction not feasible

--- Day 20/mysnake.py
+++ Day 20/mysnake.py
@@ -5,3 +5,23 @@ class Snake:
     def __init__(self):
         self.segments=[]
         self.create_snake()
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            segment = Turtle("square")
+            segment.color("white")
+            segment.penup()
+            segment.goto(position)
+            self.segments.append(segment)
+
+    def move(self):
+        for segment in self.segments:
+            if not current_pos:
+                segment.setheading(90)
+                segment.forward(20)
+                current_pos = [segment.xcor, segment.ycor]
+                set_heading
+            else:
+                segment.setpos(current_pos)
+                segment.setheading
+
--- Day 20/snakegame.py
+++ Day 20/snakegame.py
@@ -10,41 +10,41 @@ screen.title("Snake")
 game_state = True
 
 def up(segments):
-    catchup=0
+    current_pos=[]
+    heading = 0
     for segment in segments:
-        segment.forward(catchup)
-        segment.setheading(90)
-        segment.forward(20)
-        catchup+=20
+        if not current_pos:
+            segment.setheading(90)
+            segment.forward(20)
+            current_pos=[segment.xcor,segment.ycor]
+            set_heading
+        else:
+            segment.setpos(current_pos)
+            segment.setheading
 
-def left(segments):
-    catchup=0
-    for segment in segments:
-        segment.forward(catchup)
-        segment.setheading(180)
-        segment.forward(20)
-        catchup+=20
-
-def down(segments):
-    catchup=0
-    for segment in segments:
-        segment.forward(catchup)
-        segment.setheading(270)
-        segment.forward(20)
-        catchup+=20
-
-def right(segments):
-    catchup=0
-    for segment in segments:
-        segment.forward(catchup)
-        segment.setheading(0)
-        segment.forward(20)
-        catchup+=20
-
-def direction(state):
-    if state == "right":
-
-
+# def left(segments):
+#     catchup=0
+#     for segment in segments:
+#         segment.forward(catchup)
+#         segment.setheading(180)
+#         segment.forward(20)
+#         catchup+=20
+#
+# def down(segments):
+#     catchup=0
+#     for segment in segments:
+#         segment.forward(catchup)
+#         segment.setheading(270)
+#         segment.forward(20)
+#         catchup+=20
+#
+# def right(segments):
+#     catchup=0
+#     for segment in segments:
+#         segment.forward(catchup)
+#         segment.setheading(0)
+#         segment.forward(20)
+#         catchup+=20
 
 while game_state:
     screen.update()
