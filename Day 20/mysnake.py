2cec2be008a61373b6f8895ce6a2326d4ee1835c
Paul <paul.eun.shin@gmail.com>
2023-03-30 00:40:34 -0700

    Day 22

--- Day 20/mysnake.py
+++ Day 20/mysnake.py
@@ -1,10 +1,16 @@
+from turtle import Turtle
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 class Snake:
 
     def __init__(self):
         self.segments=[]
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
@@ -14,14 +20,34 @@ class Snake:
             segment.goto(position)
             self.segments.append(segment)
 
+    # def move(self):
+    #     starting = [self.segments[0].xcor(),self.segments[0].ycor()]
+    #     self.segments[0].forward(MOVE_DISTANCE)
+    #     for segment in range(1,len(self.segments)):
+    #         current_pos = [starting[0], starting[1]]
+    #         old_pos = [self.segments[segment].xcor(), self.segments[segment].ycor()]
+    #         self.segments[segment].goto(current_pos[0],current_pos[1])
+    #         current_pos = old_pos
+
     def move(self):
-        for segment in self.segments:
-            if not current_pos:
-                segment.setheading(90)
-                segment.forward(20)
-                current_pos = [segment.xcor, segment.ycor]
-                set_heading
-            else:
-                segment.setpos(current_pos)
-                segment.setheading
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num-1].xcor()
+            new_y = self.segments[seg_num-1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.segments[0].setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.segments[0].setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.segments[0].setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.segments[0].setheading(RIGHT)
 
--- Day 20/snakegame.py
+++ Day 20/snakegame.py
@@ -8,20 +8,22 @@ screen.bgcolor("black")
 screen.title("Snake")
 
 game_state = True
+snake = Snake()
 
-def up(segments):
-    current_pos=[]
-    heading = 0
-    for segment in segments:
-        if not current_pos:
-            segment.setheading(90)
-            segment.forward(20)
-            current_pos=[segment.xcor,segment.ycor]
-            set_heading
-        else:
-            segment.setpos(current_pos)
-            segment.setheading
-
+###This does not work
+# def up(segments):
+#     current_pos=[]
+#     heading = 0
+#     for segment in segments:
+#         if not current_pos:
+#             segment.setheading(90)
+#             segment.forward(20)
+#             current_pos=[segment.xcor,segment.ycor]
+#             set_heading
+#         else:
+#             segment.setpos(current_pos)
+#             segment.setheading
+#
 # def left(segments):
 #     catchup=0
 #     for segment in segments:
@@ -46,18 +48,18 @@ def up(segments):
 #         segment.forward(20)
 #         catchup+=20
 
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
 while game_state:
     screen.update()
     time.sleep(.1)
-    current_direction = "right"
-    new_direction = "right"
-    while new_direction == current_direction:
+    snake.move()
 
 
 screen.onkey()
-
-
-
-
-
 screen.exitonclick()
