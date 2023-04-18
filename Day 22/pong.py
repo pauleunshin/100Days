6bcaf3d1db7c2794fcada1ed5aa43d5cb22636f3
Paul <paul.eun.shin@gmail.com>
2023-04-18 00:49:57 -0700

    Day 22 start

---
+++ Day 22/pong.py
@@ -0,0 +1,16 @@
+# Class separation
+# paddles
+# scoreboard plus line
+# Ball
+
+from turtle import Screen, Turtle
+# from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("PONG")
+
+
+
